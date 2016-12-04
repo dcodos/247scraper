c5fa66a05d437098087f9b9166f5b7f122ed79b2
Daniel Codos <dbc5ba@virginia.edu>
2016-12-04 00:08:43 -0500

    Start with 2006

--- scrape_info.py
+++ scrape_info.py
@@ -152,5 +152,5 @@ if __name__ == "__main__":
     # print(result)
     print_header()
     print("============================================")
-    for cur_year in range(2004, 2018):
+    for cur_year in range(2006, 2018):
         run_full_year(cur_year)
