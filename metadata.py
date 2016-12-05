3c6dde8c280b6d95f5d324459ace8fe12b8cee00
Daniel Codos <dbc5ba@virginia.edu>
2016-12-05 14:34:38 -0500

    Updated metadata

--- metadata.py
+++ metadata.py
@@ -1,7 +1,19 @@
 import os
 
+def get_num_players():
+    files = os.listdir("output")
+    total = 0
+    for file in files:
+        if "info" in file:
+            with open("output/" + file) as f:
+                for i, l in enumerate(f):
+                    pass
+            total += i + 1
+    return total
 
 
 if __name__ == "__main__":
-    info_metadata = get_info_metadata()
-    interest_metadata = get_interests_metadata()
+    num_players = get_num_players()
+    # interest_metadata = get_interests_metadata()
+
+    print("Total number of players: " + str(num_players))
