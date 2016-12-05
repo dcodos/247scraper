de47c35de07edeb50abb3d340ea2c53307065624
Daniel Codos <dbc5ba@virginia.edu>
2016-12-05 14:40:49 -0500

    Updated metadata

--- metadata.py
+++ metadata.py
@@ -1,11 +1,24 @@
 import os
 
-def get_num_players():
-    files = os.listdir("output")
+BASE = "output"
+
+
+def get_num_players(files):
     total = 0
     for file in files:
         if "info" in file:
-            with open("output/" + file) as f:
+            with open(BASE + "/" + file) as f:
+                for i, l in enumerate(f):
+                    pass
+            total += i + 1
+    return total
+
+
+def get_num_interests(files):
+    total = 0
+    for file in files:
+        if "interest" in file:
+            with open(BASE + "/" + file) as f:
                 for i, l in enumerate(f):
                     pass
             total += i + 1
@@ -13,7 +26,10 @@ def get_num_players():
 
 
 if __name__ == "__main__":
-    num_players = get_num_players()
-    # interest_metadata = get_interests_metadata()
+    all_files = os.listdir(BASE)
+    num_players = get_num_players(all_files)
+    num_interests = get_num_interests(all_files)
 
-    print("Total number of players: " + str(num_players))
+    print("Total number of recruits: " + str(num_players))
+    print("Total number of interests: " + str(num_interests))
+    print("Average interests per recruit: " + str(num_interests / num_players))
